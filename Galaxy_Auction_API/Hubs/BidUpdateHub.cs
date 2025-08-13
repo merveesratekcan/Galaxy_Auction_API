46055bea6d6e283eadc2c0a1555f329ecf4bc622
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2025-08-13 23:06:36 +0300

    Hub icerisindeki kullanicilarin guncel verilere ulasmasi

--- Galaxy_Auction_API/Hubs/BidUpdateHub.cs
+++ Galaxy_Auction_API/Hubs/BidUpdateHub.cs
@@ -1,15 +1,19 @@
 ﻿using Galaxy_Auction_API.Hubs.ConnectionManagement;
 using Galaxy_Auction_Business.Dtos;
+using Galaxy_Auction_Data_Access.Context;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Galaxy_Auction_API.Hubs;
 
 public class BidUpdateHub:Hub
 {
     private readonly IConnectionManager _connectionManager;
-    public BidUpdateHub(IConnectionManager connectionManager)
+    private readonly ApplicationDbContext _context;
+    public BidUpdateHub(IConnectionManager connectionManager, ApplicationDbContext context)
     {
         _connectionManager = connectionManager;
+        _context = context;
     }
 
     public override async Task OnConnectedAsync()
@@ -22,9 +26,17 @@ public class BidUpdateHub:Hub
         await base.OnConnectedAsync();
 
     }
-    public async Task NewBid()
+    public async Task NewBid(int vehicleId)
     {
-        await Clients.Clients(new List<string>).SendAsync("messageReceived","deneme");
+        if (vehicleId != null)
+        {
+            var result = await _context.Bids
+                        .Where(b => b.VehicleId == vehicleId).ToListAsync();
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1000);
+            var connedtionId = _connectionManager.GetAllConnectionIds();
+            await Clients.Clients(connedtionId).SendAsync("messageReceived", result);
+        }
     }
 
     public override Task OnDisconnectedAsync(Exception? exception)
@@ -32,7 +44,6 @@ public class BidUpdateHub:Hub
         var connectionId = Context.ConnectionId;
         _connectionManager.RemoveConnection(connectionId);
         return base.OnDisconnectedAsync(exception);
-
     }
 
 
